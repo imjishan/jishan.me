d0ad4b453c99da77b0551930ed875b570bb76874
Jishan <129368844+imjishan@users.noreply.github.com>
2024-06-06 12:00:12 +0530

    Update header.tsx

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
     <header className="flex flex-col md:flex-row md:justify-between md:items-center space-y-4 md:space-y-0 w-full md:px-6 max-w-3xl animate-slide-from-down-and-fade-1">
       <div className="flex flex-row items-center space-x-3">
         <Image
-          src={CONFIG.headshot}
+          src="jishan.jpg"
           alt=""
           width={100}
           height={100}
